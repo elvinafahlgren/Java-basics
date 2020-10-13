2cb3f3b8cd40a175c3b09e9b6c220fca6537f07d
elvinafahlgren <elvina.fahlgren@live.com>
2020-10-13 15:18:25 +0200

    upg2week5

--- week5/src/exercises/ex2usemoreclasses/Point.java
+++ week5/src/exercises/ex2usemoreclasses/Point.java
@@ -5,15 +5,46 @@ import static java.lang.Math.sqrt;
 
 /*
 
-  A class for points in 3D. Should be immutable
+  A class for points in 3D. Should be immutable (final)
 
-  NOTE: No IO here, this is just the logical concept.
+  NOTE: No IO (input, output) here, this is just the logical concept.
 
   To test run Ex2TestPointTriangle
 
 */
 public class Point {
     // TODO
+    private final int a;
+    private final int b;
+    private final int c;
+
+    public Point(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point(Point p){
+        this.a = p.getA();
+        this.b = p.getB();
+        this.c = p.getC();
+    }
+
+    private int getA(){
+        return this.a;
+    }
+    private int getB(){
+        return this.b;
+    }
+    private int getC(){
+        return this.c;
+    }
+
+    int distance(Point p){
+        int totalP1 = this.a + this.b + this.c;
+        int totalP2 = p.getA() + p.getB() + p.getC();
+        return -1 * (totalP1 - totalP2);
+    }
 }
 
 
--- week5/src/exercises/ex2usemoreclasses/Triangle.java
+++ week5/src/exercises/ex2usemoreclasses/Triangle.java
@@ -1,7 +1,10 @@
 package exercises.ex2usemoreclasses;
 
+import exercises.ex1useaclass.Rational;
+
 import java.util.Arrays;
 
+import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 /*
@@ -14,8 +17,42 @@ import static java.lang.Math.sqrt;
 
 */
 public class Triangle {
+    private final Point p1;
+    private final Point p2;
+    private final Point p3;
 
-    // TODO
+    public Triangle(Point p1, Point p2, Point p3){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+    private Point getP1(){
+        return this.p1;
+    }
+    private Point getP2(){
+        return this.p2;
+    }
+    private Point getP3(){
+        return this.p3;
+    }
+
+    public double area(){
+        double a = p1.distance(this.p2);
+        double b = p1.distance(this.p3);
+        double c = p2.distance(this.p3);
+        double A = 0.25 * sqrt(pow((4 * pow(a, 2) * pow(b, 2)- ((pow(a, 2) + pow(b, 2) - pow(c, 2)))), 2));
+
+        return A;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Triangle t = (Triangle) o;
+        if(t.getP1() == this.p1 && t.getP2() == this.p1 && t.getP3() == this.p3)
+            return true;
+        return false;
+    }
+
 
 }
 
